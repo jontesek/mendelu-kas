d2b4b181d01066e5505ee94406384ddc46378cec
Jonas Petrovsky <jond@post.cz>
2016-01-02 11:55:04 +0100

    String and file hashing works.

--- HashProject/src/Sha1Algo.py
+++ HashProject/src/Sha1Algo.py
@@ -1,14 +1,11 @@
-import os.path
 import struct
-import io
-import pprint
-import operator
+import os.path
 
 
 class Sha1Algo(object):
 
     def __init__(self):
-        # Initial h variables
+        # Initial H variables
         self.h_values = (
             0x67452301,
             0xEFCDAB89,
@@ -18,36 +15,43 @@ class Sha1Algo(object):
         )
 
     def hash_text(self, input_text):
+        """Public method for hashing given text."""
         # Convert text to bytes (utf8).
         utf8_codes = self._convert_text_to_utf8_codes(input_text)
         text_bytes = bytearray(utf8_codes)
         # Hash the bytes.
         return self._hash_bytes(text_bytes)
 
-    def _convert_text_to_utf8_codes(self, input_text):
-        utf8_input = unicode(input_text, 'utf-8').encode('utf-8')
-        return [ord(c) for c in utf8_input]
+    def hash_file(self, file_path):
+        """Public method for hashing given file."""
+        try:
+            # Read file to array of bytes.
+            file_obj = open(os.path.abspath(file_path), 'rb')
+            file_bytes = bytearray(file_obj.read())
+            file_obj.close()
+            # Hash the bytes.
+            return self._hash_bytes(file_bytes)
+        except IOError:
+            print('The file ' + file_path + ' was not found.')
 
     def _hash_bytes(self, byte_array):
+        """Main private method for hashing (both strings and files)."""
         # Prepare and chunk message.
         prepared_msg = self._prepare_msg(byte_array)
         chunks = self._chunk_msg(prepared_msg)
         h_values = self.h_values
         # Process all chunks
         for i, chunk in enumerate(chunks):
-            print('======CHUNK %d======') % i
+            #print('======CHUNK %d======') % i
             h_values = self._process_chunk(chunk, h_values)
-        quit()
-        # Convert last h values to hexadecimal numbers.
-        hex_values = [hex(x)[2:-1].zfill(8) for x in h_values]
-        # Join the numbers together
-        final_hash = ''.join(hex_values)
+        # Produce digest from final h values.
+        final_hash = self._produce_hex_digest(h_values)
         # result
         print('>>>DIGEST<<<\n' + final_hash)
         return final_hash
 
     def _prepare_msg(self, msg):
-        """Prepares message (bytes) for dividing into chunks."""
+        """Prepares message (bytes) before dividing into chunks."""
         # Save original message length (in bytes).
         msg_byte_length = len(msg)
         # Append bit 1 to the end.
@@ -75,22 +79,64 @@ class Sha1Algo(object):
         # Above can be also written as: [msg[i:i+size] for i in range(0, len(msg), size)]
 
     def _process_chunk(self, chunk, h_values):
+        """Process a chunk of data and return new H values."""
         # Split chunk into 16 32-bit words ... 16 x 4 bytes integers
-        size = 4
-        w = [0] * 80
-        # I will just use 4 bytes as an integer.
+        words = [0] * 80
         for i in range(16):
-            w[i] = struct.unpack(b'>I', chunk[i*4:i*4 + 4])[0]
+            words[i] = struct.unpack(b'>I', chunk[i*4:i*4 + 4])[0]
         # Extend 16 words into 80 words.
         for i in range(16, 80):
-            w[i] = self._left_rotate(w[i-3] ^ w[i-8] ^ w[i-14] ^ w[i-16], 1)
+            words[i] = self._left_rotate(words[i-3] ^ words[i-8] ^ words[i-14] ^ words[i-16], 1)
+        # Initialize variables
+        a = h_values[0]
+        b = h_values[1]
+        c = h_values[2]
+        d = h_values[3]
+        e = h_values[4]
+        # Process all words
+        for i, word in enumerate(words):
+            if 0 <= i <= 19:
+                f = (b & c) | (~b & d)
+                k = 0x5A827999
+            elif 20 <= i <= 39:
+                f = b ^ c ^ d
+                k = 0x6ED9EBA1
+            elif 40 <= i <= 59:
+                f = (b & c) | (b & d) | (c & d)
+                k = 0x8F1BBCDC
+            elif 60 <= i <= 79:
+                f = b ^ c ^ d
+                k = 0xCA62C1D6
+            # Update letters
+            a, b, c, d, e = ((self._left_rotate(a, 5) + f + e + k + word) & 0xffffffff, a, self._left_rotate(b, 30), c, d)
 
+        # Calculate new H values (truncate the ones longer than 32 bits)
+        updated_h_values = [
+            (h_values[0] + a) & 0xffffffff,
+            (h_values[1] + b) & 0xffffffff,
+            (h_values[2] + c) & 0xffffffff,
+            (h_values[3] + d) & 0xffffffff,
+            (h_values[4] + e) & 0xffffffff,
+        ]
 
+        return updated_h_values
 
-    def _bytes_to_bin_string(self, byte_array):
-        new_vals = [bin(x)[2:].zfill(8) for x in byte_array]
-        return ''.join([str(x) for x in new_vals])
 
     def _left_rotate(self, n, b):
         """Left rotate a 32-bit integer n by b bits."""
         return ((n << b) | (n >> (32 - b))) & 0xffffffff
+
+    def _produce_hex_digest(self, h_values):
+        """Produce the final digest (hash value) as a string from HEX numbers."""
+        hex_values = [hex(x)[2:-1].zfill(8) for x in h_values]
+        return ''.join(hex_values)
+
+    def _convert_text_to_utf8_codes(self, input_text):
+        """Convert given text to a list of utf8 codes of its characters."""
+        utf8_input = unicode(input_text, 'utf-8').encode('utf-8')
+        return [ord(c) for c in utf8_input]
+
+    def _bytes_to_bin_string(self, byte_array):
+        """Helper method for debugging."""
+        new_vals = [bin(x)[2:].zfill(8) for x in byte_array]
+        return ''.join([str(x) for x in new_vals])
--- HashProject/test.py
+++ HashProject/test.py
@@ -4,13 +4,14 @@ from src.Sha1Algo import Sha1Algo
 sa = Sha1Algo()
 # Short text: 8f0c0855915633e4a7de19468b3874c8901df043
 #sa.hash_text('A Test')
-sa.hash_text('Příliš žluťoučký kůň úpěl ďábelské ódy')  # 8f8d965195fdaf4375fd5ad58e0a1f91329ad30f
+#sa.hash_text('Příliš žluťoučký kůň úpěl ďábelské ódy')  # 8f8d965195fdaf4375fd5ad58e0a1f91329ad30f
 
 # Long text: 100 bytes: c6d7501f54c1099833b6b19558b48dc2fc84c14c
 # sa.hash_text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel massa sem. Nullam commodo nullam')
 
 # Small file: 302 bytes: 0293b9ad9d2468e758ea72e2aef39cd3c74cdfc2
-# sa.hash_file('tests/h_file.txt')
+#sa.hash_file('tests/h_file.txt')
 # Medium file: 57 kB...57 679 bytes: 8304d2221bdb3f3498eccad4436a2c98a3be5c25
-# sa.hash_file("tests/h_file.pdf")
-# -> aaah it's too slow (15 secs)
+#sa.hash_file("tests/h_file.pdf")
+# Large file: 3 MB ... ok, but takes about 10 seconds
+#sa.hash_file('C:/img.jpg')
